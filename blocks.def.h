0c9ee90b22142ccd6c7c37a97b361c3c34f5dd80
juancortelezzi <juanbicortelezzi@gmail.com>
2021-07-13 10:28:31 -0300

    first commit

---
+++ blocks.def.h
@@ -0,0 +1,9 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", "/home/wiz/.config/dwmblocks/blocks/date-time.sh",					30,		0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
