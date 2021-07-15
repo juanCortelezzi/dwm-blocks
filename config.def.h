7f63e22f7232a9fd9e85e86a7690b299a2e0c7b0
juancortelezzi <juanbicortelezzi@gmail.com>
2021-07-15 19:03:02 -0300

    added spaces for icons

--- config.def.h
+++ config.def.h
@@ -1,8 +1,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"蓼", "/home/wiz/.config/dwmblocks/blocks/sound.sh",			 0,	10},
-	{"", "/home/wiz/.config/dwmblocks/blocks/brightness.sh",	 0,	20},
+	{"蓼 ", "/home/wiz/.config/dwmblocks/blocks/sound.sh",			 0,	10},
+	{" ", "/home/wiz/.config/dwmblocks/blocks/brightness.sh",	 0,	20},
 	{"", "/home/wiz/.config/dwmblocks/blocks/date-time.sh",		30,	 0},
 };
 
