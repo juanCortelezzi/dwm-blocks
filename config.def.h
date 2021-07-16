36d3795ea68ac02309090a05393cb19f56ba8934
juancortelezzi <juanbicortelezzi@gmail.com>
2021-07-15 23:14:14 -0300

    added memory block

--- config.def.h
+++ config.def.h
@@ -1,9 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"蓼 ", "/home/wiz/.config/dwmblocks/blocks/sound.sh",			 0,	10},
+	{" ", "/home/wiz/.config/dwmblocks/blocks/memory.sh", 120,	0},
+	{"蓼 ", "/home/wiz/.config/dwmblocks/blocks/sound.sh",		 0,	10},
 	{" ", "/home/wiz/.config/dwmblocks/blocks/brightness.sh",	 0,	20},
-	{"", "/home/wiz/.config/dwmblocks/blocks/date-time.sh",		30,	 0},
+	{"", "/home/wiz/.config/dwmblocks/blocks/date-time.sh",		60,	 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
