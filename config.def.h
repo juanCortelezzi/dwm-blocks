04e0489751865ba644ae17e0c7ccf7f55957696f
juancortelezzi <juanbicortelezzi@gmail.com>
2021-07-23 02:21:01 -0300

    memory time interval 120->60

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" ", "/home/wiz/.config/dwmblocks/blocks/memory.sh", 120,	0},
+	{" ", "/home/wiz/.config/dwmblocks/blocks/memory.sh", 60,	0},
 	{"蓼 ", "/home/wiz/.config/dwmblocks/blocks/sound.sh",		 0,	10},
 	{" ", "/home/wiz/.config/dwmblocks/blocks/brightness.sh",	 0,	20},
 	{"", "/home/wiz/.config/dwmblocks/blocks/date-time.sh",		60,	 0},
