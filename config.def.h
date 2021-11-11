df60442f2caf3fc4bd20e03e5ce922270061c597
juancortelezzi <juanbicortelezzi@gmail.com>
2021-11-11 16:20:44 -0300

    removed whitespace

--- config.def.h
+++ config.def.h
@@ -1,7 +1,6 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	     /*Update Signal*/
-	{" ", "",                                            0,	   0},
 	{"", "~/.config/dwmblocks/blocks/memory.sh",        60,	   0},
 	{"", "~/.config/dwmblocks/blocks/sound.sh",		     0,	  10},
 	{"", "~/.config/dwmblocks/blocks/brightness.sh",	 0,	  20},
