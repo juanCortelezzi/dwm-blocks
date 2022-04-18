f4d9518789cacc32e37ab8823cf2030c416969b2
J.B.C <juanbicortelezzi@gmail.com>
2022-04-17 22:20:47 -0300

    changed script directory

--- config.def.h
+++ config.def.h
@@ -1,10 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	     /*Update Signal*/
-	{"", "~/.config/dwmblocks/blocks/memory.sh",        60,	   0},
-	{"", "~/.config/dwmblocks/blocks/sound.sh",		     0,	  10},
-	{"", "~/.config/dwmblocks/blocks/brightness.sh",	 0,	  20},
-	{"", "~/.config/dwmblocks/blocks/date-time.sh",		30,	   0},
+	{"", "~/.config/suckless/dwm-blocks/blocks/memory.sh", 60, 0},
+	{"", "~/.config/suckless/dwm-blocks/blocks/sound.sh", 0, 10},
+	{"", "~/.config/suckless/dwm-blocks/blocks/battery.sh", 60, 0},
+	{"", "~/.config/suckless/dwm-blocks/blocks/brightness.sh", 0, 20},
+	{"", "~/.config/suckless/dwm-blocks/blocks/date-time.sh", 30, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
